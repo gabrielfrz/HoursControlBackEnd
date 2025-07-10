f3db5acf8457bc36edf4297d7eaae9fd056f1220
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-07-10 12:28:18 +0000

    Cors

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 import pointRoutes from './routes/point.routes.js';
 import { connectDB } from './database/db.js';
 
 const app = express();
+
+// Habilitar CORS para Vercel e Codespaces
+app.use(cors({
+  origin: [
+    'https://hours-control-front-end.vercel.app',
+    'https://turbo-space-telegram-qr7xgg76pw7hxpjj-3000.app.github.dev'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Conectar banco
