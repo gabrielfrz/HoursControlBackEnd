8d284a052aa2c4a7b3826c1a67260b70b1f68872
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-07-11 16:02:55 +0000

    Fix Vercel 11

--- api/database/db.js
+++ api/database/db.js
@@ -1,20 +1,23 @@
 import pg from "pg";
-console.log("DATABASE_URL carregado no db.js:", process.env.DATABASE_URL);
 
 const { Pool } = pg;
 
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 export const connectDB = async () => {
   try {
     const client = await pool.connect();
-    console.log("Conectado ao PostgreSQL via Neon!");
-    client.release();
+    console.log("Conexão com o PostgreSQL estabelecida com sucesso!");
+    client.release(); // Libera o cliente de volta para o pool.
   } catch (error) {
-    console.error("Erro ao conectar ao DB:", error);
-    process.exit(1);
+    console.error("Falha ao conectar com a base de dados:", error);
   }
 };
