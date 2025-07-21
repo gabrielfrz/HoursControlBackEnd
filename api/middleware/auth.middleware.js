3cc8fa25c5f1bb3f1a59dda4c08a08f2f51e118d
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-07-21 16:02:48 +0000

    Fixing Middleware (1)

--- api/middleware/auth.middleware.js
+++ api/middleware/auth.middleware.js
@@ -3,11 +3,15 @@ import jwt from "jsonwebtoken";
 export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+
+  if (!token) return res.status(401).json({ message: "Token não fornecido." });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403);
-    req.userId = decoded.id; // 
+    if (err) return res.status(403).json({ message: "Token inválido." });
+
+    req.user = decoded;          
+    req.userId = decoded.id;     
+
     next();
   });
 };
