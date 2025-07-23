aee0df32dbb76addb3b2d31f97a8a7492efd62b1
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-07-23 12:26:07 +0000

    >

--- api/index.js
+++ api/index.js
@@ -43,5 +43,5 @@ app.get("/api/health", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta : ${PORT}`);
 });
