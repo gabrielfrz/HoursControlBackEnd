64530830c036530117dde37c5ee4d6c3b27808b6
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-07-24 15:05:07 +0000

    Ajuste CORS 2

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-  const dotenv = await import('dotenv');
-  dotenv.config();
-}
+import 'dotenv/config'; 
 
 import express from "express";
 import cors from "cors";
@@ -21,17 +18,15 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      console.log("CORS bloqueado para origem:", origin);
       callback(new Error("Origem não permitida pelo CORS"));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", userRoutes);
